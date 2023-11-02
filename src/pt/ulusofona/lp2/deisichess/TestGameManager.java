5d17e7553df5925d219e7fe73400ced43e877a39
Miguel Melo <a21905215@alunos.ulht.pt>
2023-11-02 17:27:21 +0000

    Import do JUnit5

---
+++ src/pt/ulusofona/lp2/deisichess/TestGameManager.java
@@ -0,0 +1,11 @@
+package pt.ulusofona.lp2.deisichess;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestGameManager {
+}
