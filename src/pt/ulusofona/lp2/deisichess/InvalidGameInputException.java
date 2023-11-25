5e1a631edb01bc5014cd008ce54e1a8bfb457245
Miguel Melo <a21905215@alunos.ulht.pt>
2023-11-25 16:11:28 +0000

    Criação da Classe de Exceptions

---
+++ src/pt/ulusofona/lp2/deisichess/InvalidGameInputException.java
@@ -0,0 +1,21 @@
+package pt.ulusofona.lp2.deisichess;
+
+public class InvalidGameInputException {
+
+
+    public int getLinewithError() {
+
+        /*RETORNA NUMERO DA LINHA DO FICHEIRO QUE TEM O PRIMEIRO ERRO*/
+
+
+        return 1;
+    }
+
+    public String getProblemDescription(int problemType) {
+
+        /*RETORNA DESCRICAO DO ERRO*/
+
+        return "inshallah";
+    }
+
+}
