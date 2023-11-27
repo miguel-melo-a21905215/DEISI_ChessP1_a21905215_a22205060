b7e5d44dac442d1dc80ccc476a299122803d8a1f
Miguel Melo <a21905215@alunos.ulht.pt>
2023-11-27 12:50:23 +0000

    New class - GameHistory, self-explanatory

---
+++ src/pt/ulusofona/lp2/deisichess/GameHistory.java
@@ -0,0 +1,12 @@
+package pt.ulusofona.lp2.deisichess;
+
+import java.util.ArrayList;
+
+/*TODO - CONVERTER PARA ALGO UTILIZAVEL NO SAVE*/
+/*GUARDA OS MOVES FEITOS POR CADA PEÇA*/
+public class GameHistory {
+
+
+
+
+}
