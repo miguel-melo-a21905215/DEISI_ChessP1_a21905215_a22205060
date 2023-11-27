9adf277a4e765d6525a48fed8f0ab75069bdfb85
Gonçalo Barata <goncalo.costa.barata@gmail.com>
2023-11-27 23:37:44 +0100

    move rainha atualizado

--- src/pt/ulusofona/lp2/deisichess/Rainha.java
+++ src/pt/ulusofona/lp2/deisichess/Rainha.java
@@ -4,8 +4,9 @@ public class Rainha extends Piece {
 
     public Rainha(int id, int team, String nickname, Board board) {
         super(id,10, team, nickname); //aqui n sei o que e suposto por no type
-        
+
     }
+
 
     @Override
     public boolean move(int oriX, int oriY, int destX, int destY, Board board) {
@@ -15,22 +16,24 @@ public class Rainha extends Piece {
             return false;
         }
 
+        if(board.temPeca(oriX, oriY) && (destX != oriX || destY != oriY)) {
 
-        if (board.temPeca(oriX, oriY) && (destX != oriX || destY != oriY)) {
             Piece pecaMovida = board.getPecaNaPos(oriX, oriY);
 
+            if(pecaMovida.isInPlay() && pecaMovida.getTeam() == board.isCurrentTeamNumb()
+                    && board.validaMove(oriX, oriY, destX, destY)) {
 
-            if (pecaMovida.isInPlay() && pecaMovida.getTeam() == board.isCurrentTeamNumb() &&
-                    (board.validaMove(oriX, oriY, destX, destY))) {
+                if(board.temPeca(destX, destY)) {
 
-
-                if (board.temPeca(destX, destY)) {
                     Piece pecaNoDestino = board.getPecaNaPos(destX, destY);
 
+                    if(pecaNoDestino.getTeam() != board.isCurrentTeamNumb()){
 
-                    if (pecaNoDestino.getTeam() != board.isCurrentTeamNumb()) {
+                        int distanciaX = destX - oriX;
+                        int distanciaY = destY - oriY;
 
-                        if (Math.abs(destX - oriX) <= 5 && Math.abs(destY - oriY) <= 5 &&
+                        if(distanciaX <= 5 && distanciaX >= -5 &&
+                                distanciaY <= 5 && distanciaY >= -5 &&
                                 !board.temPecaNoCaminho(oriX, oriY, destX, destY)) {
 
                             pecaNoDestino.capturada();
@@ -38,32 +41,46 @@ public class Rainha extends Piece {
                             board.comeu();
                             board.tiraPecaOrigem(oriX, oriY);
                             return true;
+
                         } else {
                             board.falhou();
                             return false;
                         }
+
                     } else {
+
                         board.equipas[board.isCurrentTeamNumb()].invalida();
                         return false;
+
                     }
+
                 } else {
 
-                    if (Math.abs(destX - oriX) <= 5 && Math.abs(destY - oriY) <= 5 &&
+                    int distanciaX = destX - oriX;
+                    int distanciaY = destY - oriY;
+
+                    if(distanciaX <= 5 && distanciaX >= -5 &&
+                            distanciaY <= 5 && distanciaY >= -5 &&
                             !board.temPecaNoCaminho(oriX, oriY, destX, destY)) {
 
                         board.tiraPecaOrigem(oriX, oriY);
                         board.metePecaDestino(pecaMovida, destX, destY);
                         board.moveu();
                         return true;
+
                     } else {
                         board.falhou();
                         return false;
                     }
+
                 }
+
             }
+
         }
 
         board.falhou();
         return false;
+
     }
 }
