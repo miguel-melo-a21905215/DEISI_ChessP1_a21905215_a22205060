299b01e05cd81b6088da55529dcf72ab5be4a0ca
Miguel Melo <a21905215@alunos.ulht.pt>
2023-12-01 19:18:23 +0000

    Rei's specificMoveValidation

--- src/pt/ulusofona/lp2/deisichess/Rei.java
+++ src/pt/ulusofona/lp2/deisichess/Rei.java
@@ -9,4 +9,12 @@ public class Rei extends Piece {
         this.inPlay = false;
         this.typeStr = "Rei";
     }
+
+    @Override
+    public boolean specificMoveValidation(int oriX, int oriY, int destX, int destY, Piece[][] tabuleiro) {
+        int deslocX = Math.abs(destX - oriX);
+        int deslocY = Math.abs(destY - oriY);
+
+        return (deslocX == 1 && deslocY == 0) || (deslocX == 0 && deslocY == 1) || (deslocX == 1 && deslocY == 1);
+    }
 }
