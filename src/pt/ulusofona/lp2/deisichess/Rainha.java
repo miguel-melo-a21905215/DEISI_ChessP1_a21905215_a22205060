25e4ac7f5eeb86f551b15fc5903eb6d9c5770d7b
Gonçalo Barata <goncalo.costa.barata@gmail.com>
2023-12-04 15:14:15 +0000

    specificMoveValidation for rainha, first try

--- src/pt/ulusofona/lp2/deisichess/Rainha.java
+++ src/pt/ulusofona/lp2/deisichess/Rainha.java
@@ -17,68 +17,22 @@ public class Rainha extends Piece {
     @Override
     public boolean specificMoveValidation(int oriX, int oriY, int destX, int destY, Piece[][] tabuleiro) {
 
-        /*NAO PRECISAMOS DE TER AQUI A VERIFICA COORDENADAS DENTRO DO TABULEIRO,
-         * PASSEI PARA A CHAMADA DO MOVE NA GAMEMANAGER PORQUE É COMUM A TODAS AS
-         * PEÇAS. ADICIONEI MAIS CONDIÇÕES QUE TAMBÉM SÃO COMUNS*/
+        int deltaX = Math.abs(destX - oriX);
+        int deltaY = Math.abs(destY - oriY);
 
-       /*if (!board.coordenadasDentroTabuleiro(oriX, oriY) || !board.coordenadasDentroTabuleiro(destX, destY)) {
-            board.falhou();
-            return false;
-        }
+        if ((deltaX == 0 && deltaY <= 5) || (deltaY == 0 && deltaX <= 5)) {
 
-
-        if (board.temPeca(oriX, oriY) && (destX != oriX || destY != oriY)) {
-            Piece pecaMovida = board.getPecaNaPos(oriX, oriY);
-
-
-            if (pecaMovida.isInPlay() && pecaMovida.getTeam() == board.isCurrentTeamNumb() &&
-                    (board.validaMoveGeral(oriX, oriY, destX, destY))) {
-
-
-                if (board.temPeca(destX, destY)) {
-                    Piece pecaNoDestino = board.getPecaNaPos(destX, destY);
-
-
-                    if (pecaNoDestino.getTeam() != board.isCurrentTeamNumb()) {
-
-                        if ((destX - oriX <= 5 || oriX - destX <= 5) &&
-                                (destY - oriY <= 5 || oriY - destY <= 5) &&
-                                !board.temPecaNoCaminho(oriX, oriY, destX, destY)) {
-
-                            pecaNoDestino.capturada();
-                            board.metePecaDestino(pecaMovida, destX, destY);
-                            board.comeu();
-                            board.tiraPecaOrigem(oriX, oriY);
-                            return true;
-
-                        } else {
-                            board.falhou();
-                            return false;
-                        }
-                    } else {
-                        board.equipas[board.isCurrentTeamNumb()].invalida();
-                        return false;
-                    }
-                } else {
-
-                    if ((destX - oriX) <= 5 && (destX - oriX) >= -5 &&
-                            (destY - oriY) <= 5 && (destY - oriY) >= -5 &&
-                            !board.temPecaNoCaminho(oriX, oriY, destX, destY)) {
-
-                        board.tiraPecaOrigem(oriX, oriY);
-                        board.metePecaDestino(pecaMovida, destX, destY);
-                        board.moveu();
-                        return true;
-
-                    } else {
-                        board.falhou();
-                        return false;
-                    }
-                }
+            if (deltaX > 0) {
+                return lineCheckForPieces(oriX, oriY, destX, tabuleiro);
+            } else if (deltaY > 0) {
+                return columnCheckForPieces(oriX, oriY, destY, tabuleiro);
             }
+
+        } else if (deltaX == deltaY && deltaX <= 5) {
+
+            return diagonalCheckForPieces(oriX, oriY, destX, destY, tabuleiro);
         }
 
-        board.falhou();*/
         return false;
     }
 }
