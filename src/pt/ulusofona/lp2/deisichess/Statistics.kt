7980e2232adf552ea58e1797c874d1a83ce84096
Gonçalo Barata <goncalo.costa.barata@gmail.com>
2023-12-19 22:46:46 +0000

    trying to do getTiposPecaCapturados and getPecasMais5Capturas, to be correct in DP

--- src/pt/ulusofona/lp2/deisichess/Statistics.kt
+++ src/pt/ulusofona/lp2/deisichess/Statistics.kt
@@ -7,7 +7,7 @@ package pt.ulusofona.lp2.deisichess;
         StatType.TOP_5_PONTOS -> return :: getTopPontos
         StatType.PECAS_MAIS_5_CAPTURAS -> return :: getPecasMais5Capturas
         StatType.PECAS_MAIS_BARALHADAS -> return :: getTopPontos
-        StatType.TIPOS_CAPTURADOS -> return :: getTopPontos
+        StatType.TIPOS_CAPTURADOS -> return :: getTiposPecaCapturados
     }
  }
 
@@ -38,17 +38,35 @@ package pt.ulusofona.lp2.deisichess;
     return pieces
  }
 
-fun getPecasMais5Capturas(gameManager: GameManager): ArrayList<String> {
+ fun getPecasMais5Capturas(gameManager: GameManager): ArrayList<String> {
     val pieces = gameManager.board.totalPieces
         .filter { it.numCapturas > 5 }
         .sortedByDescending { it.numCapturas }
         .take(5)
         .map { piece ->
-            "${piece.nickname} (${if (piece.team == 10) "PRETA" else "BRANCA"}) " +
-                    "fez ${piece.numCapturas} capturas"
+            "${if (piece.team == 10) "PRETA" else "BRANCA"}:${piece.nickname}:${piece.numCapturas}"
         }
         .toCollection(ArrayList())
 
     return pieces
-}
+ }
+
+
+
+
+
+ fun getTiposPecaCapturados(gameManager: GameManager): ArrayList<String> {
+
+        val piecesCaptured = gameManager.board.totalPieces.filter { !it.isInPlay() }
+        val namesCaptured = piecesCaptured.map { piece ->
+            piece.typeStr
+        }.distinct()
+
+        return ArrayList(namesCaptured)
+ }
+
+
+
+
+
 
